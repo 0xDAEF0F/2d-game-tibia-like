fce4c6b2f2af742dcd8f3d54a5ceb067eb7b51ea
Alex T <aletapia@proton.me>
2025-01-28 11:06:23 -0700

    added boundary checking on the client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -7,8 +7,7 @@ use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::net::UdpSocket;
-use tokio::sync::mpsc;
-use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::sync::mpsc::{self, UnboundedReceiver};
 use tokio::task::JoinHandle;
 
 const TILE_WIDTH: f32 = 32.0;
@@ -31,6 +30,27 @@ impl Player {
         let x = self.curr_location.0 as f32 * TILE_WIDTH;
         let y = self.curr_location.1 as f32 * TILE_HEIGHT;
         draw_rectangle(x, y, TILE_WIDTH, TILE_HEIGHT, RED);
+    }
+
+    pub fn can_move((x, y): (i32, i32), op: &OtherPlayers) -> bool {
+        let horizontal_tiles = (screen_width() / TILE_WIDTH) as i32 - 1;
+        let vertical_tiles = (screen_height() / TILE_HEIGHT) as i32 - 1;
+
+        if x.is_negative() || y.is_negative() {
+            return false;
+        }
+
+        if x > horizontal_tiles || y > vertical_tiles {
+            return false;
+        }
+
+        for &(px, py) in op.0.values() {
+            if (px, py) == (x as usize, y as usize) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
@@ -133,7 +153,7 @@ async fn draw(socket: Arc<UdpSocket>, mut rx: UnboundedReceiver<Message>) {
         other_players.render();
 
         // Handle movement
-        handle_player_movement(&mut player);
+        handle_player_movement(&mut player, &other_players);
 
         // Send player state to server if changed
         send_new_pos_to_server(&mut player, &socket);
@@ -165,7 +185,7 @@ fn send_new_pos_to_server(player: &mut Player, socket: &UdpSocket) {
     _ = socket.try_send(&serialized_message);
 }
 
-fn handle_player_movement(player: &mut Player) {
+fn handle_player_movement(player: &mut Player, op: &OtherPlayers) {
     let current_time = get_time();
     let can_move = current_time - player.last_move_timer >= player.speed.into();
 
@@ -176,41 +196,62 @@ fn handle_player_movement(player: &mut Player) {
     let mut keys_down = get_keys_down();
 
     if keys_down.len() == 1 {
-        handle_single_key_movement(player, keys_down.drain().next().unwrap(), current_time);
+        handle_single_key_movement(player, op, keys_down.drain().next().unwrap(), current_time);
     } else if keys_down.len() == 2 {
-        handle_double_key_movement(player, current_time);
+        handle_double_key_movement(player, op, current_time);
     }
 }
 
-fn handle_single_key_movement(player: &mut Player, key: KeyCode, current_time: f64) {
+fn handle_single_key_movement(
+    player: &mut Player,
+    op: &OtherPlayers,
+    key: KeyCode,
+    current_time: f64,
+) {
+    let (x, y) = player.curr_location;
+    let (x, y) = (x as i32, y as i32);
     match key {
-        KeyCode::Right if player.curr_location.0 < (screen_width() / TILE_WIDTH) as usize - 1 => {
+        KeyCode::Right if Player::can_move((x + 1, y), op) => {
             move_player(player, (1, 0), current_time, BASE_MOVE_DELAY);
         }
-        KeyCode::Left if player.curr_location.0 > 0 => {
+        KeyCode::Left if Player::can_move((x - 1, y), op) => {
             move_player(player, (-1, 0), current_time, BASE_MOVE_DELAY);
         }
-        KeyCode::Up if player.curr_location.1 > 0 => {
+        KeyCode::Up if Player::can_move((x, y - 1), op) => {
             move_player(player, (0, -1), current_time, BASE_MOVE_DELAY);
         }
-        KeyCode::Down if player.curr_location.1 < (screen_height() / TILE_HEIGHT) as usize - 1 => {
+        KeyCode::Down if Player::can_move((x, y + 1), op) => {
             move_player(player, (0, 1), current_time, BASE_MOVE_DELAY);
         }
         _ => {}
     }
 }
 
-fn handle_double_key_movement(player: &mut Player, current_time: f64) {
-    if is_key_down(KeyCode::Right) && is_key_down(KeyCode::Up) {
+fn handle_double_key_movement(player: &mut Player, op: &OtherPlayers, current_time: f64) {
+    let (x, y) = player.curr_location;
+    let (x, y) = (x as i32, y as i32);
+    if is_key_down(KeyCode::Right)
+        && is_key_down(KeyCode::Up)
+        && Player::can_move((x + 1, y - 1), op)
+    {
         move_player(player, (1, -1), current_time, BASE_MOVE_DELAY * 2.0);
     }
-    if is_key_down(KeyCode::Right) && is_key_down(KeyCode::Down) {
+    if is_key_down(KeyCode::Right)
+        && is_key_down(KeyCode::Down)
+        && Player::can_move((x + 1, y + 1), op)
+    {
         move_player(player, (1, 1), current_time, BASE_MOVE_DELAY * 2.0);
     }
-    if is_key_down(KeyCode::Left) && is_key_down(KeyCode::Up) {
+    if is_key_down(KeyCode::Left)
+        && is_key_down(KeyCode::Up)
+        && Player::can_move((x - 1, y - 1), op)
+    {
         move_player(player, (-1, -1), current_time, BASE_MOVE_DELAY * 2.0);
     }
-    if is_key_down(KeyCode::Left) && is_key_down(KeyCode::Down) {
+    if is_key_down(KeyCode::Left)
+        && is_key_down(KeyCode::Down)
+        && Player::can_move((x - 1, y + 1), op)
+    {
         move_player(player, (-1, 1), current_time, BASE_MOVE_DELAY * 2.0);
     }
 }
